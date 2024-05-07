7209e38463204c8370b14ab34461fe64e11632b3
VasuBB <vasuofficial2005@gmail.com>
2024-05-07 16:23:33 +0530

    Setting up chakra

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-
+import {Button} from '@chakra-ui/react'
 function App() {
   
-  return <>hi</>
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<HomePage />}/>
+      </Routes>
+    </>
+  )
 }
 
 export default App
